89c42171d32446c29b1afaef1210ca950444b80f
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2021-09-13 12:29:23 +0200

    oop for server

--- server.h
+++ server.h
@@ -1,10 +1,30 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <thread>
+#include <list>
+#include <string>
+
+using namespace std;
+
 class Server {
 private:
+  int s;
+
 public:
   Server() = default;
+  void run();
 };
 
 #endif
